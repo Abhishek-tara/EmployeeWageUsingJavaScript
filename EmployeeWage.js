a7ff423961abf0bd3da0508ec4d440e62e2470ef
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-28 12:59:05 +0530

    Added ObjectCreation

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,8 +6,8 @@ const NUM_OF_WORKING_DAYS = 20;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const WAGE_PER_HOUR = 20;
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
+//let totalEmpHrs = 0;
+//let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
 
 function calcDailyWage(empHrs) {
@@ -134,4 +134,28 @@ empDailyHrsMap.forEach((value, key, map) => {
 console.log("Full Working Days:" + fullWorkingDays);
 console.log("Part Working Days:" + partWorkingDays);
 console.log("Non Working Days:" + nonWorkingDays);
+
+//UC-10---->Ability to store the Day, Hours Worked and Wage Earned in a single object
+let totalEmpHrs = 0
+let totalWorkingDays = 0;
+let empDailyHrsAndWageArr = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+    totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsAndWageArr.push(
+        {
+            dayNum: totalWorkingDays,
+            dailyHours: empHrs,
+            dailyWage: calcDailyWage(empHrs),
+            toString() {
+                return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours +
+                    'And Wage Earmed= ' + this.dailyWage
+            },
+        });
+
+}
+console.log("UC-10 Showing Daily Hours Worked and Wage Earned: " + empdailyHrsAndWageArr);
 
